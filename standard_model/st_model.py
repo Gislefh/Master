606701a72afb8d0d366c772f4718698ba6ca0b60
Gisle <gislefh@stud.ntnu.no>
2019-05-15 11:48:45 +0200

    added ls for water jet model parametrs

--- standard_model/st_model.py
+++ standard_model/st_model.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import least_squares
 
 ### --- jet model ---
 def jet_model(nu, jet_rpm, delta_nozzle):
@@ -141,11 +142,93 @@ def sys(states, tau_b):
 	return out
 
 
+
+
+
+def ls_wj(X, sys):
+
+	#params = [a0, a1, a2, r0, r1, r2]
+	def new_wj_model(params, nu, jet_rpm, delta_nozzle):
+		#constants
+		lever_CGtowj_port = [-3.82, -0.475]
+		lever_CGtowj_stb = [-3.82, 0.475]
+
+		#rpm2thrust
+		speed = nu[0]		 ###* 1.94384 # knots no need
+		a0 = params[0]
+		a1 = params[1]
+		a2 = params[2]
+		thrust_unscaled = a0 + a1*speed + a2*(speed**2)
+
+		r0 = params[3]
+		r1 = params[4]
+		r2 = params[5]
+
+		rpm_scale = r0 + r1*jet_rpm + r2 * (jet_rpm **2) # no need to scale
+
+		thrust = rpm_scale * thrust_unscaled
+
+
+		#waterjet port
+		Fx = thrust*np.cos(delta_nozzle)
+		Fy = thrust*np.sin(delta_nozzle)
+
+		#moment
+		Nz_port = (lever_CGtowj_port[0]*Fy)- (lever_CGtowj_port[1]*Fx)
+		Nz_stb = (lever_CGtowj_stb[0]*Fy)- (lever_CGtowj_stb[1]*Fx)
+
+		tau_b =  [2*Fx, 2*Fy, Nz_port + Nz_stb]
+
+		return tau_b
+
+	def fun(params):
+		eta_dot_nu_dot = np.zeros((6, len(X[-1])))
+		for i in range(np.shape(X)[1]):
+			tau_b = new_wj_model(params, X[0:3, i], X[-4, i], X[-3, i]) # nu, jet_rpm, delta_nozzle
+
+			states = np.append(X[6, i], X[0:3, i])
+
+			eta_dot_nu_dot[:, i] = np.squeeze(sys(states, tau_b))
+
+		#du = eta_dot_nu_dot[3]
+		#dv = eta_dot_nu_dot[4]
+		#dr = eta_dot_nu_dot[5]
+
+		#optimize for du
+		all_st = True
+		just_du = False
+
+		if just_du:
+			sq_err = ((X[3] - eta_dot_nu_dot[3])**2)
+			mse = np.sum(sq_err)/len(sq_err)
+			print('Mean Squared Error:', mse)
+			return sq_err
+
+		if all_st:
+			sq_err = np.add(np.add(((X[3] - eta_dot_nu_dot[3])**2),((X[4] - eta_dot_nu_dot[4])**2)), ((X[5] - eta_dot_nu_dot[5])**2))
+			mse = np.sum(sq_err)/len(sq_err)
+			print('Mean Squared Error:', mse)
+			return sq_err
+
+
+
+	x0 = [1000,-100,1,10,-1,0.01]
+	sol = least_squares(fun, x0)
+	print(sol.x)
+
+
+
+
 ### ---load data---
 path = '/home/gislehalv/Master/scripts/standard_model/'
 
 #X = load_data(path)
 X = np.load(path +'Data_cut01.npy')
+
+#X = np.delete(X, list(range(1000, np.shape(X)[1])), axis = 1)
+
+ls_wj(X, sys)
+
 
 ### ---Simulate---
 tau_b = np.zeros((3, len(X[-1])))
@@ -205,14 +288,14 @@ plt.grid()
 
 plt.subplot(312)
 plt.plot(X[-1], X[4])
-plt.plot(X[-1], eta_dot_nu_dot[4]) ## minus passer bedre? hmmm, passer godt med *(-1/2)
+plt.plot(X[-1], eta_dot_nu_dot[4] *(-1/2)) ## minus passer bedre? hmmm, passer godt med *(-1/2)
 plt.ylabel('dv')
 plt.legend(['data', 'model'])	
 plt.grid()
 
 plt.subplot(313)
 plt.plot(X[-1], X[5])
-plt.plot(X[-1], eta_dot_nu_dot[5]) ## minus passer bedre? hmmm, passer godt med *(-1/10)
+plt.plot(X[-1], eta_dot_nu_dot[5] *(-1/10)) ## minus passer bedre? hmmm, passer godt med *(-1/10)
 plt.ylabel('dr')
 plt.legend(['data', 'model'])
 plt.grid()
